5e6adb075829f6f2be7c432f10f21fcd96046d65
Bilol <madiyarovbilol78@gmail.com>
2025-02-17 20:51:56 +0500

    Jasurbek bajargan team bo'limi merge qilindi

--- controller/cars.controller.js
+++ controller/cars.controller.js
@@ -1,9 +1,10 @@
 import { getData } from "../services/cars.service.js";
 
 let getHome = (req, res) => {
-    let datas = getData('data')
-    res.render('index', {title: "Avtomobil xizmatlari", datas: datas})
-}
+  let datas = getData("data")
+  res.render("index", { title: "Avtomobil xizmatlari", datas: datas });
+};
+
 
 let getAbout = (req, res) => {
   res.render("about", { title: "Avtomobil xizmatlari" });
@@ -17,4 +18,29 @@ let getbooking = (req, res) => {
   res.render("booking", { title: "Avtomobil xizmatlari" });
 };
 
-export { getHome, getAbout, getService, getbooking };
+let team = (req, res) => {
+  res.render("team", { title: "Avtomobil xizmatlari" });
+};
+
+let gettestimonial = (req, res) => {
+  res.render("testimonial", { title: "Avtomobil xizmatlari" });
+};
+
+let getContact = (req, res) => {
+  res.render("contact", { title: "Avtomobil xizmatlari" });
+};
+
+let get404 = (req, res) => {
+  res.render("404", { title: "Avtomobil xizmatlari" });
+};
+
+export {
+  getHome,
+  getAbout,
+  getService,
+  getbooking,
+  team,
+  gettestimonial,
+  getContact,
+  get404,
+};
--- routes/cars.route.js
+++ routes/cars.route.js
@@ -4,6 +4,10 @@ import {
   getAbout,
   getService,
   getbooking,
+  team,
+  gettestimonial,
+  getContact,
+  get404,
 } from "../controller/cars.controller.js";
 
 let route = Router();
@@ -12,6 +16,10 @@ route
   .get("/", getHome)
   .get("/about", getAbout)
   .get("/service", getService)
-  .get("/booking", getbooking);
+  .get("/booking", getbooking)
+  .get("/team", team)
+  .get("/testimonial", gettestimonial)
+  .get("/contact", getContact)
+  .get("/404", get404);
 
 export default route;
